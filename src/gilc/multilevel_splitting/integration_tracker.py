05902f3764debf84f8f42c9f2ea1c7673202aab1
agessner <agessner@tue.mpg.de>
2019-12-10 17:18:01 +0100

    improved the integration tracker

--- src/gilc/multilevel_splitting/integration_tracker.py
+++ src/gilc/multilevel_splitting/integration_tracker.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-from ..core.loop_state import LoopState
+from .. import LoopState
 
 
 class IntegratorState(LoopState):
     def __init__(self):
+        super().__init__()
         self.nestings = []
 
     def add_nesting(self, new_nesting):
@@ -20,18 +21,28 @@ class IntegratorState(LoopState):
         Checks if the nesting sequence is complete and covers the failure domain
         :return: Boolean
         """
-        if len(self.sequence_of_nestings) == 0:
-            return False
-        return True if self.sequence_of_nestings[-1].shift == 0. else False
+        return NotImplementedError
 
     def integral(self):
-        return self.nesting_factors.prod()
+        return self.conditional_probabilities.prod()
 
     def log_integral(self):
-        return self.log_nesting_factors.sum()
+        return self.log_conditional_probabilities.sum()
 
     def log2_integral(self):
-        return self.log2_nesting_factors.sum()
+        return self.log2_conditional_probabilities.sum()
+
+    @property
+    def conditional_probabilities(self):
+        return np.exp(self.log_conditional_probabilities)
+
+    @property
+    def log2_conditional_probabilities(self):
+        return self.log_conditional_probabilities / np.log(2.)
+
+    @property
+    def log_conditional_probabilities(self):
+        return np.asarray([nest.log_conditional_probability for nest in self.nestings])
 
 
 class HDRTracker(IntegratorState):
@@ -40,100 +51,31 @@ class HDRTracker(IntegratorState):
         Track record of HDR method given a sequence of shifts
         :param shift_sequence: sequence of shifts for HDR method (as np.ndarray)
         """
-        super(SubsetSimulationTracker, self).__init__()
+        super().__init__()
         self.shift_sequence = shift_sequence
-        self.log_nesting_factors = self._get_log_nesting_factors()
-
-    def add_nesting(self, hdr_nesting):
-        """
-        Add a nesting to the track record
-        :param hdr_nesting: Instance of HDRNesting
-        :return: None
-        """
-        self.nestings.append(hdr_nesting)
-        self.log_nesting_factors = self._get_log_nesting_factors()
-        return
 
     def is_complete(self):
         return len(self.shift_sequence) == len(self.nestings)
-
-    def integral(self):
-        return self.nesting_factors.prod()
-
-    def log_integral(self):
-        return self.log_nesting_factors.sum()
-
-    def log2_integral(self):
-        return self.log2_nesting_factors.sum()
-
-    @property
-    def nesting_factors(self):
-        return np.exp(self.log_nesting_factors)
-
-    @property
-    def log2_nesting_factors(self):
-        return self.log_nesting_factors / np.log(2.)
-
-    def _get_log_nesting_factors(self):
-        return np.asarray([nest.log_conditional_probability for nest in self.nestings])
-
 
 
 class SubsetSimulationTracker(IntegratorState):
     def __init__(self):
         super(SubsetSimulationTracker, self).__init__()
 
-    def integral(self):
-        """
-        Compute the integral of the smallest nesting
-        :return: Integral value
-        """
-        integral = 1.
-        for nest in self.sequence_of_nestings:
-            integral *= nest.conditional_probability
-        return integral
-
-    def log_integral(self):
-        """
-        Compute the logarithm of the integral
-        :return: Log integral value
-        """
-        log_integral = 0.
-        for nest in self.sequence_of_nestings:
-            log_integral += np.log(nest.conditional_probability)
-        return log_integral
-
-    def log2_integral(self):
-        """
-        Compute the logarithm to base 2 of the integral
-        :return: Log2 integral value
-        """
-        log2_integral = 0.
-        for nest in self.sequence_of_nestings:
-            log2_integral += np.log2(nest.conditional_probability)
-        return log2_integral
-
-    def inner_domain_samples(self):
-        """
-        Returns samples from the smallest nesting
-        :return: Samples of what is currently the inner nesting
-        """
-        return self.sequence_of_nestings[-1].X_inside()
-
     def is_complete(self):
         """
         Checks if the nesting sequence is complete and covers the failure domain
         :return: Boolean
         """
-        if len(self.sequence_of_nestings) == 0:
+        if len(self.nestings) == 0:
             return False
-        return True if self.sequence_of_nestings[-1].shift == 0. else False
+        return True if self.nestings[-1].shift == 0. else False
 
     def n_nestings(self):
         """
         :return: Number of nestings
         """
-        return len(self.sequence_of_nestings)
+        return len(self.nestings)
 
     @property
     def shift_sequence(self):
@@ -141,8 +83,8 @@ class SubsetSimulationTracker(IntegratorState):
         All shifts in the nesting sequence
         :return: array of shifts
         """
-        return np.asarray([nest.shift for nest in self.sequence_of_nestings])
+        return np.asarray([nest.shift for nest in self.nestings])
 
     def x_inits(self):
-        """ Initial locations of all nestings """
-        return np.hstack([nest.x_in for nest in self.sequence_of_nestings])
+        """ Initial locations needed for sampling from the nestings """
+        return np.hstack([nest.x_in for nest in self.nestings])
